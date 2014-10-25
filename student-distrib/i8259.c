eefcd881a7f9dfd1fa4fc2c570dc9fe77aa2172e
sagarwl4 <sagarwl4@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-10-25 03:40:00 +0000

    IDT works - getting double faults

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -22,6 +22,13 @@
 #define ICW1_ICW4	0x01		/* ICW4 (not) needed */
 #define ICW4_8086	0x01		/* 8086/88 (MCS-80/85) mode */
 
+//some extra definations 
+// #define ICW1 		0x11
+// #define ICW2_MASTER	0x21
+// #define ICW2_SLAVE	0x28
+// #define ICW3_MASTER	0x04
+// #define ICW3_SLAVE	0x02
+// #define ICW4 		0x01
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* Interrupt masks to determine which interrupts
  * are enabled and disabled */
@@ -32,42 +39,37 @@ uint8_t slave_mask; /* IRQs 8-15 */
 void
 i8259_init(void)
 {
-	unsigned char a1;
-	unsigned char a2;
+	 unsigned char a1;
+	 unsigned char a2;
  
-	a1 = inb(PIC1_DATA);                        // save masks
-	a2 = inb(PIC2_DATA);
+	 a1 = inb(PIC1_DATA);                        // save masks
+	 a2 = inb(PIC2_DATA);
  
-	outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+  	outb(PIC1_COMMAND, ICW1);  					// starts the initialization sequence (in cascade mode)
+	outb(PIC2_COMMAND, ICW1);					// we can even define it as ICW1 instead of the addition
+
+	outb(PIC1_DATA, ICW2_MASTER);               // ICW2: Master PIC vector offset
+	outb(PIC2_DATA, ICW2_SLAVE);   				// ICW2: Slave PIC vector offset
+
+	outb(PIC1_DATA, ICW3_MASTER); 				// ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	outb(PIC2_DATA, ICW3_SLAVE);    			// ICW3: tell Slave PIC its cascade identity (0000 0010)
+ 
+	outb(PIC1_DATA, ICW4);
+	outb(PIC2_DATA, ICW4);
+ 
+ 	outb(PIC1_DATA, a1);   						// restore saved masks.
+	outb(PIC2_DATA, a2);
+/*	There are some wait statemets which come below outb commands. Refer to http://wiki.osdev.org/8259_PIC for exact code and some of the code is below*/
+
 	//io_wait();			in case you want to use it, #include <io.h>
 	/*io_wait() checks the descriptors that the program is interested in to see whether any of them are ready. If none of them are ready, io_wait() tries to pause until one of them is ready, so that it does not take time away from other programs running on the same computer.
-io_wait pays attention to timeouts: if a descriptor reaches its timeout, and the program is interested in reading or writing that descriptor, io_wait will return promptly.
+	io_wait pays attention to timeouts: if a descriptor reaches its timeout, and the program is interested in reading or writing that descriptor, io_wait will return promptly.
 
-Under some circumstances, io_wait will return even though no interesting descriptors are ready. Do not assume that a descriptor is ready merely because io_wait has returned.
+	Under some circumstances, io_wait will return even though no interesting descriptors are ready. Do not assume that a descriptor is ready merely because io_wait has returned.
 
-io_wait is not interrupted by the delivery of a signal. Programs that expect interruption are unreliable: they will block if the same signal is delivered a moment before io_wait. The correct way to handle signals is with the self-pipe trick.
-*/	
-	outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
-	//io_wait();
-	outb(PIC1_DATA, master_mask);                 // ICW2: Master PIC vector offset
-	//io_wait();
-	outb(PIC2_DATA, slave_mask);                 // ICW2: Slave PIC vector offset
-	//io_wait();
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	//io_wait();
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
-	//io_wait();
- 
-	outb(PIC1_DATA, ICW4_8086);
-	//io_wait();
-	outb(PIC2_DATA, ICW4_8086);
-	//io_wait();
- 
-	outb(PIC1_DATA, a1);   // restore saved masks.
-	outb(PIC2_DATA, a2);
-
-	printf(" MURALI WANTS SO MUCH MO ");
-
+	io_wait is not interrupted by the delivery of a signal. Programs that expect interruption are unreliable: they will block if the same signal is delivered a moment before io_wait. The correct way to handle signals is with the self-pipe trick.
+*/
+//	printf(" MURALI WANTS SO MUCH MO ");
 }
 
 /* Enable (unmask) the specified IRQ */
@@ -114,10 +116,18 @@ disable_irq(uint32_t irq_num)
 void
 send_eoi(uint32_t irq_num)
 {
-	if(irq_num >= 8)
-		outb(PIC2_COMMAND,PIC_EOI);
- 
-	outb(PIC1_COMMAND,PIC_EOI);
+		if(irq_num >= 8)
+	{
+		uint32_t temp_irq_num = irq_num - 8;
+		unsigned char temp_eoi = EOI | temp_irq_num; 
+		outb(temp_eoi, PIC2);
+ 		outb(EOI | 2, PIC1);		
+	}	
+	else
+	{
+		unsigned char temp_eoi = EOI | irq_num;
+		outb(temp_eoi, PIC1);
+	}	
 }
 
 void
@@ -150,3 +160,4 @@ init_rtc(void)
 //}
 
 //////////@#$%^&*(&^%&^#*^$*&^%(%(*^*%^@)* OEFUI#&* #r))
+
--- student-distrib/int_handler.h
+++ student-distrib/int_handler.h
@@ -19,4 +19,46 @@ void rtc_int_handler();
 /* Dummy Interrupt handler */
 void dummy_int_handler();
 
+void set_idt();
+
+void set_exception();
+
+void set_interrupt();
+
+void divide_error();
+
+void NMI_Interrupt();
+
+void breakpoint();
+
+void overflow();
+
+void BOUND_range_exceeded();
+
+void invalid_opcode();
+
+void device_not_available();
+
+void double_fault();
+
+void coprocessor_segment_overrun();
+
+void invalid_TSS();
+
+void segment_not_present();
+
+void stack_segment_fault();
+
+void general_protection();
+
+void page_fault();
+
+void floating_point_error();
+
+void alignment_check();
+
+void machine_check();
+
+void floating_point_exception();
+
 #endif
