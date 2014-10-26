bddfd87942691353898bdc5afe2330fc89678258
sagarwl4 <sagarwl4@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-10-26 21:32:52 +0000

    everything seems to be working

--- _projects/Fireball/student-distrib/rtc.c
+++ _projects/Fireball/student-distrib/rtc.c
@@ -1,2 +1,17 @@
 #include "rtc.h"
+#include "lib.h"
 
+/* RTC Interrupt handler */
+/********************************************************
+void													*
+rtc_int_handler()										*	
+*	Inputs:			void								*
+*	Outputs:		writes video memory to screen 	 	*
+*	Return Value:	void								*
+*	Function: Should execute test_interrupts handler	*
+********************************************************/
+void rtc_int_handler()
+{
+	printf("RTC INTERRUPT HANDLER");
+	test_interrupts();									//calls function that writes video memory to screen.
+}
