61db3643702b893bd35b89db572af1dd6474fdaf
smurali2 <smurali2@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-11-15 00:42:46 +0000

    Latest copy, code for PCB and assembly linkage added. Friday 6.42

--- _projects/Fireball/student-distrib/systemcalls.h
+++ _projects/Fireball/student-distrib/systemcalls.h
@@ -8,8 +8,8 @@
 int32_t execute(const uint8_t* command);
 //int32_t read(int32_t fd, void* buf, int32_t nbytes);
 //int32_t write(int32_t fd, const void* buf, int32_t nbytes);
-//int32_t open(const uint8_t* filename);
-//int32_t close(int32_t fd);
+int32_t open(const uint8_t* filename);
+int32_t close(int32_t fd);
 //int32_t getargs(uint8_t* buf, int32_t nbytes);
 //int32_t vidmap(uint8_t** screen_start);
 //int32_t set_handler(int32_t signum, void* handler_address);
@@ -17,9 +17,8 @@ int32_t execute(const uint8_t* command);
 
 /*	Helper functions for execute(const uint8_t* command)	*/
 char* parse(char* input);
-void stdin_jmp_table(int32_t fd);
-void stdout_jmp_table(int32_t fd);
-
+void get_arg(int i, char* input);
+int32_t empty(void);
 /***********************************************************************/
 /*	PROCESS CONTROL BLOCK	*/
 /***********************************************************************/
@@ -31,12 +30,14 @@ typedef struct file_array{
 	uint32_t flags;						//this flag tells us which fds are available
 }file_array_t;
 
+
 //PCB structure 
 typedef struct pcb{
-//	file_array_t file_fds;				//array of open files are represented with a file array defined above
+	file_array_t file_fds[8];			//array of open files are represented with a file array defined above
 	uint32_t fd;						//integer index into this array is called a file descriptor and this integer is how user-level programs identify the open file
 	uint32_t parent_bp;					//keep track of parent process' base pointer
 	uint32_t parent_sp;					//keep track of parent prcoess' stack pointer
+	uint8_t process_id;
 //	uint32_t 
 //	uint32_t
 }pcb_t;
