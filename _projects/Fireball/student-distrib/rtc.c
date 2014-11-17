4c4e186dc6c73859a45bfbd755ab0f22ec5c0935
jtlang2 <jtlang2@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-11-17 07:05:38 +0000

    BROKEN CODE: base code to change teminal read/write

--- _projects/Fireball/student-distrib/rtc.c
+++ _projects/Fireball/student-distrib/rtc.c
@@ -42,7 +42,7 @@ test_rtc()												*
 void test_rtc()
 {
 	if(interrupt_number > 20)
-		rtc_write(64);
+		rtc_write(NULL, 64);
 	if(test_flag!=0)
 		printf("%d", interrupt_number);
 }
@@ -55,23 +55,12 @@ void disable_rtc_test()
 int32_t rtc_open()
 {	//printf("inside rtc_open\n");
 	//init RTC to 2Hz. Then return 0 - should i write the rate as in i8259.c again or is that sufficient
-	int rate = 0xF;									// rate must be above 2 and not over 15
-	disable_irq(8);
-	outb(0x8A, 0x70);								// set index to register A, disable NMI
-	char prev = inb(0x71);							// get initial value of register A
-	outb(0x8A, 0x70);								// reset index to A
-	outb((prev & 0xF0) | rate, 0x71); 				//write only our rate to A. Note, rate is the bottom 4 bits.
-
-	outb(0x8B, 0x70);								// select register B, and disable NMI
-	prev = inb(0x71);								// read the current value of register B
-	outb(0x8B, 0x70);								// set the index again (a read will reset the index to register D)
-	outb(prev | 0x40, 0x71);						// write the previous value ORed with 0x40. This turns on bit 6 of register B
-	enable_irq(8);									//the offset of the RTC is IRQ1	
+								//the offset of the RTC is IRQ1	
 //	printf("finished rtc_open\n");
 	return 0;
 }
 
-int32_t rtc_read()
+int32_t rtc_read(char* buf, int count)
 {	//printf("inside rtc_read\n");
 	//For RTC, this call should always return 0, but only after an interrupt has occurred.
 	//set a flag and wait until the interrupt handler clears it then return 0
@@ -89,7 +78,7 @@ int32_t rtc_read()
 demonstrate that you can change the clock frequency
 Probably make a function call in kernel to show that frequency can be changed
 *************************************************/
-int32_t rtc_write(uint32_t frequency)
+int32_t rtc_write(char* buf, int32_t frequency)
 {	//printf("inside rtc_write\n");
 //In the case of the RTC, the system call should always accept only a 4-byte
 // 		integer specifying the interrupt rate in Hz, and should set the rate of periodic interrupts accordingly.
@@ -120,18 +109,7 @@ int32_t rtc_close()
 // The close system call closes the specified file descriptor and makes it available for return from later calls to open.
 // 	You should not allow the user to close the default descriptors (0 for input and 1 for output). Trying to close an invalid
 // 		descriptor should result in a return value of -1; successful closes should return 0.
-	int rate = 0xF;									// rate must be above 2 and not over 15
-	disable_irq(8);
-	outb(0x8A, 0x70);								// set index to register A, disable NMI
-	char prev = inb(0x71);							// get initial value of register A
-	outb(0x8A, 0x70);								// reset index to A
-	outb((prev & 0xF0) | rate, 0x71); 				//write only our rate to A. Note, rate is the bottom 4 bits.
-
-	outb(0x8B, 0x70);								// select register B, and disable NMI
-	prev = inb(0x71);								// read the current value of register B
-	outb(0x8B, 0x70);								// set the index again (a read will reset the index to register D)
-	outb(prev | 0x40, 0x71);						// write the previous value ORed with 0x40. This turns on bit 6 of register B
-	enable_irq(8);									//the offset of the RTC is IRQ1	
+							//the offset of the RTC is IRQ1	
 	//printf("finished rtc_close\n");	
 	return 0;
 }
